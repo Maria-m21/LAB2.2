2ffd5a2cedb61c60a41c057c270ad24b1ea381c4
oltsaaa <a.oltsaa@gmail.com>
2024-03-05 20:56:51 +0200

    Initial commit

---
+++ src/second.java
@@ -0,0 +1,8 @@
+public class second {
+    public static void main(String[] args){
+        int number = 78;
+        int sum = (number/10) + (number%10);
+
+        System.out.println(+ sum);
+    }
+}
